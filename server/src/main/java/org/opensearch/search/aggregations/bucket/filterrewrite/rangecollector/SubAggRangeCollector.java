cb08e1c286a35ac68ab1c2ae8a9d1661df664298
EC2 Default User <ec2-user@ip-172-31-53-239.us-west-2.compute.internal>
2026-04-28 19:46:58 +0000

    Cherry-pick: Adding bulk collect API for filter rewrite sub aggregation collection (#19933)

--- server/src/main/java/org/opensearch/search/aggregations/bucket/filterrewrite/rangecollector/SubAggRangeCollector.java
+++ server/src/main/java/org/opensearch/search/aggregations/bucket/filterrewrite/rangecollector/SubAggRangeCollector.java
@@ -12,7 +12,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.lucene.index.LeafReaderContext;
 import org.apache.lucene.search.DocIdSetIterator;
-import org.apache.lucene.util.BitDocIdSet;
+import org.apache.lucene.search.DocIdStreamHelper;
 import org.apache.lucene.util.FixedBitSet;
 import org.opensearch.search.aggregations.BucketCollector;
 import org.opensearch.search.aggregations.LeafBucketCollector;
@@ -22,8 +22,6 @@ import org.opensearch.search.aggregations.bucket.filterrewrite.Ranges;
 import java.io.IOException;
 import java.util.function.BiConsumer;
 import java.util.function.Function;
-
-import static org.apache.lucene.search.DocIdSetIterator.NO_MORE_DOCS;
 
 /**
  * Range collector implementation that supports sub-aggregations by collecting doc IDs.
@@ -38,7 +36,6 @@ public class SubAggRangeCollector extends SimpleRangeCollector {
     private final LeafReaderContext leafCtx;
 
     private final FixedBitSet bitSet;
-    private final BitDocIdSet bitDocIdSet;
 
     public SubAggRangeCollector(
         Ranges ranges,
@@ -53,9 +50,7 @@ public class SubAggRangeCollector extends SimpleRangeCollector {
         this.getBucketOrd = getBucketOrd;
         this.collectableSubAggregators = subAggCollectorParam.collectableSubAggregators();
         this.leafCtx = subAggCollectorParam.leafCtx();
-        int numDocs = leafCtx.reader().maxDoc();
-        bitSet = new FixedBitSet(numDocs);
-        bitDocIdSet = new BitDocIdSet(bitSet);
+        bitSet = new FixedBitSet(leafCtx.reader().maxDoc());
     }
 
     @Override
@@ -82,13 +77,9 @@ public class SubAggRangeCollector extends SimpleRangeCollector {
 
         // trigger the sub agg collection for this range
         try {
-            DocIdSetIterator iterator = bitDocIdSet.iterator();
             // build a new leaf collector for each bucket
             LeafBucketCollector sub = collectableSubAggregators.getLeafCollector(leafCtx);
-            while (iterator.nextDoc() != NO_MORE_DOCS) {
-                int currentDoc = iterator.docID();
-                sub.collect(currentDoc, bucketOrd);
-            }
+            sub.collect(DocIdStreamHelper.getDocIdStream(bitSet), bucketOrd);
             logger.trace("collected sub aggregation for bucket {}", bucketOrd);
         } catch (IOException e) {
             throw new RuntimeException(e);
